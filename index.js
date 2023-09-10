1cc57bfe38c9e2e685a7ca6651d382dcf4ec45fc
Vinlic <vinlic@vinlic.com>
2023-09-11 04:05:33 +0800

    支持帧截取

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import Pool from "./lib/Pool.js";
+import Synthesizer from "./lib/Synthesizer.js";
 
-const pool = new Pool({ numBrowserMin: 1, numBrowserMax: 1, browserOptions: { numPageMin: 1, numPageMax: 1 } });
+const pool = new Pool({
+    numBrowserMin: 1,
+    numBrowserMax: 1,
+    browserOptions: {
+        numPageMin: 1,
+        numPageMax: 1,
+        useGPU: true,
+        debug: false,
+        pageOptions: {
+            width: 1920,
+            height: 1080
+        }
+    }
+});
 
 (async () => {
+    const synthesizer = new Synthesizer();
     await pool.warmup();
-    const page = await pool.acquirePage();
-    page.goto("https://threejs.org/examples/#webgl_animation_keyframes");
-    console.log("啊啊");
+    let page = await pool.acquirePage();
+    page.onFrame(data => synthesizer.inputFrame(data));
+    page.onPad(duration => synthesizer.padDuration(duration));
+    await page.goto("https://threejs.org/examples/webgl_animation_keyframes.html");
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.startScreencast();
+    await new Promise(resolve => setTimeout(resolve, 15000));
+    await page.stopScreencast();
+    await page.release();
+    page = await pool.acquirePage();
+    console.log(page);
+    // page.onFrame();
 })()
 .catch(err => console.error(err));
