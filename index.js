e3c574afdbb95956375cb18c78debe303876ea25
Vinlic <vinlic@vinlic.com>
2023-09-12 02:36:35 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Pool from "./lib/Pool.js";
 import Synthesizer from "./lib/Synthesizer.js";
+import fs from "fs-extra";
 
 const pool = new Pool({
     numBrowserMin: 1,
     numBrowserMax: 1,
     browserOptions: {
+        headless: true,
         numPageMin: 1,
         numPageMax: 1,
         useGPU: true,
@@ -17,19 +19,29 @@ const pool = new Pool({
 });
 
 (async () => {
+    // https://animejs.com/
+    // https://dataveyes.com/en
+    // https://fifty-five.com
+    
+    await fs.remove("./test");
+    await fs.ensureDir("test");
     const synthesizer = new Synthesizer();
     await pool.warmup();
     let page = await pool.acquirePage();
+    page.onLog(message => console.log(message));
     page.onFrame(data => synthesizer.inputFrame(data));
     page.onPad(duration => synthesizer.padDuration(duration));
-    await page.goto("https://threejs.org/examples/webgl_animation_keyframes.html");
+    await page.goto("https://animejs.com/");
     await page.setViewport({ width: 1920, height: 1080 });
-    await page.startScreencast();
-    await new Promise(resolve => setTimeout(resolve, 15000));
+    await page.startScreencast({ fps: 60, duration: 20000 });
+    console.time("render");
+    await new Promise(resolve => page.onComplete(resolve));
+    console.timeEnd("render");
+    // await new Promise(resolve => setTimeout(resolve, 15000));
     await page.stopScreencast();
     await page.release();
-    page = await pool.acquirePage();
-    console.log(page);
+    // page = await pool.acquirePage();
+    // console.log(page);
     // page.onFrame();
 })()
 .catch(err => console.error(err));
