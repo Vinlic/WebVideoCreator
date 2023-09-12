3b6426d2fea2529aa62f5b2b38878c520b11f87a
Vinlic <vinlic@vinlic.com>
2023-09-13 00:38:44 +0800

    update

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import fs from "fs-extra";
 
 const pool = new Pool({
     numBrowserMin: 1,
-    numBrowserMax: 1,
+    numBrowserMax: 5,
     browserOptions: {
         headless: true,
         numPageMin: 1,
-        numPageMax: 1,
+        numPageMax: 5,
         useGPU: true,
         debug: false,
         pageOptions: {
@@ -18,30 +18,31 @@ const pool = new Pool({
     }
 });
 
-(async () => {
-    // https://animejs.com/
-    // https://dataveyes.com/en
-    // https://fifty-five.com
-    
-    await fs.remove("./test");
-    await fs.ensureDir("test");
-    const synthesizer = new Synthesizer();
-    await pool.warmup();
-    let page = await pool.acquirePage();
-    page.onLog(message => console.log(message));
-    page.onFrame(data => synthesizer.inputFrame(data));
-    page.onPad(duration => synthesizer.padDuration(duration));
-    await page.goto("https://animejs.com/");
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.startScreencast({ fps: 60, duration: 20000 });
-    console.time("render");
-    await new Promise(resolve => page.onComplete(resolve));
-    console.timeEnd("render");
-    // await new Promise(resolve => setTimeout(resolve, 15000));
-    await page.stopScreencast();
-    await page.release();
-    // page = await pool.acquirePage();
-    // console.log(page);
-    // page.onFrame();
-})()
-.catch(err => console.error(err));
+// https://animejs.com/
+// https://dataveyes.com/en
+// https://fifty-five.com
+
+await pool.warmup();
+
+for(let i = 0;i < 1;i++) {
+    (async () => {
+        const page = await pool.acquirePage();
+        page.onLog(message => console.log(message));
+        const synthesizer = new Synthesizer(`${i}`);
+        page.onFrame((data, timestamp) => synthesizer.inputFrame(data, timestamp));
+        page.onPad(duration => synthesizer.padDuration(duration));
+        await page.goto("https://threejs.org/examples/webgl_lights_spotlight.html");
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.startScreencast({ fps: 60, duration: 20000 });
+        console.time(`render${i}`);
+        await new Promise(resolve => page.onComplete(resolve));
+        console.timeEnd(`render${i}`);
+        // await new Promise(resolve => setTimeout(resolve, 15000));
+        await page.stopScreencast();
+        await page.release();
+        // page = await pool.acquirePage();
+        // console.log(page);
+        // page.onFrame();
+    })()
+    .catch(err => console.error(err));
+}
