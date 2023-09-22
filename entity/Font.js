7560229ee0ea9ab90306e1431f927bb9b7ac4d12
Vinlic <vinlic@vinlic.com>
2023-09-23 00:38:39 +0800

    字体引入调整

--- entity/Font.js
+++ entity/Font.js
@@ -6,8 +6,8 @@ import _ from "lodash";
  */
 export default class Font {
 
-    /** @type {string} - 字体来源 */
-    src;
+    /** @type {string} - 字体URL */
+    url;
     /** @type {string} - 字体集名称 */
     family;
     /** @type {string} - 字体样式 */
@@ -21,7 +21,7 @@ export default class Font {
      * 构造函数
      * 
      * @param {Object} options - 字体选项
-     * @param {string} options.src - 字体来源
+     * @param {string} [options.url] - 字体URL
      * @param {string} options.family - 字体集名称
      * @param {string} options.format - 字体格式
      * @param {string} [options.style] - 字体样式
@@ -29,17 +29,18 @@ export default class Font {
      */
     constructor(options) {
         assert(_.isObject(options), "Font options must be Object");
-        const { src, family, format, style, weight } = options;
-        assert(_.isString(src), "Font src must be string");
+        const { url, family, format, style, weight } = options;
+        assert(_.isString(url), "Font path or url must be string");
         assert(_.isString(family), "Font family must be string");
         assert(_.isString(format), "Font format must be string");
         assert(_.isNil(style) || _.isString(style), "Font style must be string");
         assert(_.isNil(weight) || _.isFinite(weight) || _.isString(weight), "Font weight must be number or string");
-        this.src = src;
+        this.url = url;
         this.family = family;
         this.format = format;
         this.style = style;
-        this.weight = parseInt(weight);
+        this.weight = _.isNumber(weight) ? parseInt(weight) : weight;
+        this.#tmpDirPath = util.rootPathJoin(`tmp/preprocessor/`);
     }
 
     /**
@@ -52,7 +53,8 @@ export default class Font {
             font-family:"${this.family}";
             ${this.style ? `font-style:${this.style};` : ""}
             ${this.weight ? `font-weight:${this.weight};` : ""}
-            src:url(${this.src})format("${this.format}")}`;
+            src:url(${this.url})format("${this.format}")
+        }`;
     }
 
 }
