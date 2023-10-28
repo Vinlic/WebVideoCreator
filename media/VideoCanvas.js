d3b7ea60fd0cf273f753681932fbaa06613e582b
Vinlic <vinlic@vinlic.com>
2023-10-28 12:54:02 +0800

    视频首帧在加载后立即绘制

--- media/VideoCanvas.js
+++ media/VideoCanvas.js
@@ -224,7 +224,7 @@ export default class VideoCanvas {
                 this.destory();
                 return false;
             }
-            this.seek(0);
+            await this.seek(0);
             return true;
         }
         catch (err) {
